1dfb40c694480d2ef42459792969faecf24548fe
MohamedHassan275 <mohamed209059@gmail.com>
2020-08-18 12:14:00 +0200

    Initial commit

--- app/src/main/java/com/mohamedhassan/kotlin_app/ListViewActivity.kt
+++ app/src/main/java/com/mohamedhassan/kotlin_app/ListViewActivity.kt
@@ -8,8 +8,8 @@ import android.widget.*
 
 class ListViewActivity : AppCompatActivity() {
 
-    lateinit var ListView : ListView
-    lateinit var ListViewPosion : TextView
+    lateinit var ListView: ListView
+    lateinit var ListViewPosion: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,17 +19,27 @@ class ListViewActivity : AppCompatActivity() {
         ListView = findViewById(R.id.ListView)
         ListViewPosion = findViewById(R.id.ListViewPosion)
 
-        var  listViewItem  = mutableListOf<Person>()
+        var listViewItem = mutableListOf<Person>()
 
-        listViewItem.add(Person("Mohamed","25","Nasr City",R.drawable.ic_launcher_background))
-        listViewItem.add(Person("Ahmed","19","Sohag City",R.drawable.ic_launcher_background))
-        listViewItem.add(Person("Mahmoud","23","Asuit City",R.drawable.ic_launcher_background))
-        listViewItem.add(Person("Ali","22","Menia City",R.drawable.ic_launcher_background))
-        listViewItem.add(Person("Islam","22","Giza City",R.drawable.ic_launcher_background))
+        listViewItem.add(Person("Mohamed", "25", "Nasr City", R.drawable.ic_launcher_background))
+        listViewItem.add(Person("Ahmed", "19", "Sohag City", R.drawable.ic_launcher_background))
+        listViewItem.add(Person("Mahmoud", "23", "Asuit City", R.drawable.ic_launcher_background))
+        listViewItem.add(Person("Ali", "22", "Menia City", R.drawable.ic_launcher_background))
+        listViewItem.add(Person("Islam", "22", "Giza City", R.drawable.ic_launcher_background))
 
-        var  adapterView = MyAdapterList(this,R.layout.my_list_view,listViewItem)
+        var adapterView = MyAdapterList(this, R.layout.my_list_view, listViewItem)
 
         ListView.adapter = adapterView
+
+        ListView.setOnItemClickListener { parent: AdapterView<*>?, view: View?, position: Int, l: Long ->
+            var NameListView = listViewItem[position]
+
+            var person: Person = listViewItem[position]
+            ListViewPosion.text = person.Address
+            Toast.makeText(this, "this position is " + person.Address, Toast.LENGTH_LONG)
+                .show()
+            // startActivity(Intent(this,MainActivity::class.java).putExtra("NameListView",NameListView))
+        }
 
 //        var ListViewName = arrayOf("Mohamed","Ahmed","Islam","Mahmoud","Hossam","Fatma","Reham")
 //        ListView.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,ListViewName)
