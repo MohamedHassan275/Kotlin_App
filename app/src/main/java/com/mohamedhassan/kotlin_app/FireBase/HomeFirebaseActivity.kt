17c49a33dfe528ec0bfba830cf3e1b4c366c9e02
MohamedHassan275 <mohamed209059@gmail.com>
2020-08-18 14:35:17 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedhassan/kotlin_app/FireBase/HomeFirebaseActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamedhassan.kotlin_app.FireBase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mohamedhassan.kotlin_app.R
+
+class HomeFirebaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_firebase)
+    }
+}
---
+++ app/src/main/java/com/mohamedhassan/kotlin_app/FireBase/LoginFirebaseActivity.kt
@@ -0,0 +1,44 @@
+package com.mohamedhassan.kotlin_app.FireBase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.mohamedhassan.kotlin_app.R
+
+class LoginFirebaseActivity : AppCompatActivity() {
+
+    lateinit var et_EmailFirebase : EditText
+    lateinit var etPasswordFirebase : EditText
+    lateinit var BtnLoginFirebase : Button
+    lateinit var btnRegisterFirebase : Button
+    var myAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_firebase)
+
+        et_EmailFirebase = findViewById(R.id.et_EmailFirebase)
+        etPasswordFirebase = findViewById(R.id.et_PasswordFirebase)
+        BtnLoginFirebase = findViewById(R.id.btn_LoginFirebase)
+        btnRegisterFirebase = findViewById(R.id.btn_RegisterFirebase)
+
+        BtnLoginFirebase.setOnClickListener {
+
+
+
+        }
+
+        btnRegisterFirebase.setOnClickListener {
+
+            SignUp()
+        }
+
+    }
+
+    private fun SignUp (){
+
+
+    }
+}
